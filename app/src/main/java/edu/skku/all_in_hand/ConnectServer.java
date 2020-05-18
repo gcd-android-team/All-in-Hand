defbf35f50253ee489fd214e99c6e26b2383c9c6
seyoung <seftware99@gmail.com>
2020-05-18 15:04:41 +0900

    [UPDATE] REST API 이용하여 GET 해오기 TEST 구현

---
+++ app/src/main/java/edu/skku/all_in_hand/ConnectServer.java
@@ -0,0 +1,17 @@
+package edu.skku.all_in_hand;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+public class ConnectServer {
+
+
+}
