82dc92264d33b3329611fa28d20b346b2a1e76e7
seyoung <seftware99@gmail.com>
2020-05-25 16:58:41 +0900

    Floating Action Button 구현

--- app/src/main/java/edu/skku/all_in_hand/ConnectServer.java
+++ app/src/main/java/edu/skku/all_in_hand/ConnectServer.java
@@ -1,11 +1,20 @@
 package edu.skku.all_in_hand;
 
 import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.lang.ref.WeakReference;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
@@ -13,5 +22,111 @@ import java.net.URL;
 
 public class ConnectServer {
 
+    /*
+    TextView showJSON;
+    Button showJSONbutton;
+    String REQUEST_URL ="http://54.180.99.61:8080/stores/test2";
+    String TAG = "get json test";
+    public static final int LOAD_SUCCESS = 101;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        showJSON = (TextView) findViewById(R.id.showJSON);
+        showJSONbutton = (Button) findViewById(R.id.showJSONbutton);
+
+        showJSONbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getJSON();
+            }
+        });
+    }
+
+    private final MyHandler mHandler = new MyHandler(this);
+
+    private static class MyHandler extends Handler {
+        private final WeakReference<MainActivity> weakReference;
+
+        public MyHandler(MainActivity mainactivity) {
+            weakReference = new WeakReference<MainActivity>(mainactivity);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            MainActivity mainactivity = weakReference.get();
+            if (mainactivity != null) {
+                switch (msg.what){
+                    case LOAD_SUCCESS:
+                        String jsonString = (String)msg.obj;
+                        mainactivity.showJSON.setText(jsonString);
+                        break;
+                }
+            }
+        }
+    }
+
+
+    public void getJSON() {
+        Thread thread = new Thread(new Runnable() {
+            public void run() {
+                String result="";
+                try {
+                    Log.d(TAG, REQUEST_URL);
+                    URL url = new URL(REQUEST_URL);
+                    HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                    httpURLConnection.setReadTimeout(3000);
+                    httpURLConnection.setConnectTimeout(3000);
+                    //httpURLConnection.setDoOutput(true);
+                    //httpURLConnection.setDoInput(true);
+                    httpURLConnection.setRequestMethod("GET");
+                    httpURLConnection.setUseCaches(false);
+                    httpURLConnection.connect();
+
+                    int responseStatusCode = httpURLConnection.getResponseCode();
+
+                    InputStream inputStream;
+                    if (responseStatusCode == HttpURLConnection.HTTP_OK) {
+                        inputStream = httpURLConnection.getInputStream();
+                    } else {
+                        inputStream = httpURLConnection.getErrorStream();
+                    }
+
+
+                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                    /*
+                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                    StringBuilder sb = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        sb.append(line);
+                    }
+                    bufferedReader.close();
+                    httpURLConnection.disconnect();
+
+                    result=sb.toString().trim();
+
+                    Log.d("Result", result);
+
+                    Message message = mHandler.obtainMessage(LOAD_SUCCESS, result);
+                    mHandler.sendMessage(message);
+                } catch (UnsupportedEncodingException | MalformedURLException ex) {
+                    ex.printStackTrace();
+                } catch (Exception e) {
+                    Log.d("ERR", e.toString());
+                    result = e.toString();
+                }
+
+
+            }
+
+        });
+        thread.start();
+    }
+
+        */
 }
--- app/src/main/java/edu/skku/all_in_hand/MainActivity.java
+++ app/src/main/java/edu/skku/all_in_hand/MainActivity.java
@@ -1,143 +1,67 @@
 package edu.skku.all_in_hand;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.util.JsonReader;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.lang.ref.WeakReference;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    TextView showJSON;
-    Button showJSONbutton;
-    String REQUEST_URL ="http://54.180.99.61:8080/stores/test2";
-    //
-    //https://jsonplaceholder.typicode.com/users
-    String TAG = "get json test";
-    public static final int LOAD_SUCCESS = 101;
+    private Context mContext;
+
+    private FloatingActionButton fab_main, fab_sub1;
+    private Animation fab_open, fab_close;
+    private boolean isFabOpen = false;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
-        showJSON = (TextView) findViewById(R.id.showJSON);
-        showJSONbutton = (Button) findViewById(R.id.showJSONbutton);
+        mContext = getApplicationContext();
 
-        showJSONbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getJSON();
-            }
-        });
+        fab_open = AnimationUtils.loadAnimation(mContext, R.anim.fab_open);
+        fab_close = AnimationUtils.loadAnimation(mContext, R.anim.fab_close);
+
+        fab_main = (FloatingActionButton) findViewById(R.id.fab_main);
+        fab_sub1 = (FloatingActionButton) findViewById(R.id.fab_sub1);
+
+        fab_main.setOnClickListener(this);
+        fab_sub1.setOnClickListener(this);
     }
 
-    private final MyHandler mHandler = new MyHandler(this);
-
-    private static class MyHandler extends Handler {
-        private final WeakReference<MainActivity> weakReference;
-
-        public MyHandler(MainActivity mainactivity) {
-            weakReference = new WeakReference<MainActivity>(mainactivity);
-        }
-
-        @Override
-        public void handleMessage(Message msg) {
-            MainActivity mainactivity = weakReference.get();
-            if (mainactivity != null) {
-                switch (msg.what){
-                case LOAD_SUCCESS:
-                    String jsonString = (String)msg.obj;
-                    mainactivity.showJSON.setText(jsonString);
-                    break;
-                }
-            }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.fab_main:
+                toggleFab();
+                break;
+            case R.id.fab_sub1:
+                toggleFab();
+                Toast.makeText(this, "Goto Mypage - 개발하장", Toast.LENGTH_SHORT).show();
+                break;
         }
     }
 
-
-    public void getJSON() {
-        Thread thread = new Thread(new Runnable() {
-            public void run() {
-                String result="";
-                try {
-                    Log.d(TAG, REQUEST_URL);
-                    URL url = new URL(REQUEST_URL);
-                    HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-
-                    httpURLConnection.setReadTimeout(3000);
-                    httpURLConnection.setConnectTimeout(3000);
-                    //httpURLConnection.setDoOutput(true);
-                    //httpURLConnection.setDoInput(true);
-                    httpURLConnection.setRequestMethod("GET");
-                    httpURLConnection.setUseCaches(false);
-                    httpURLConnection.connect();
-
-                    int responseStatusCode = httpURLConnection.getResponseCode();
-
-                    InputStream inputStream;
-                    if (responseStatusCode == HttpURLConnection.HTTP_OK) {
-                        inputStream = httpURLConnection.getInputStream();
-                    } else {
-                        inputStream = httpURLConnection.getErrorStream();
-                    }
-
-
-                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
-
-                    /*
-                    JsonReader jsonReader = new JsonReader(inputStreamReader);
-                    jsonReader.beginObject(); // Start processing the JSON object
-                    while (jsonReader.hasNext()) { // Loop through all keys
-                        String value = jsonReader.nextString();
-                        result+=value;
-                    }
-                    */
-                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-
-                    StringBuilder sb = new StringBuilder();
-                    String line;
-                    while ((line = bufferedReader.readLine()) != null) {
-                        sb.append(line);
-                    }
-                    bufferedReader.close();
-                    httpURLConnection.disconnect();
-
-                    result=sb.toString().trim();
-
-                    Log.d("Result", result);
-
-                    Message message = mHandler.obtainMessage(LOAD_SUCCESS, result);
-                    mHandler.sendMessage(message);
-                    //result = sb.toString().trim();
-                } catch (UnsupportedEncodingException | MalformedURLException ex) {
-                    ex.printStackTrace();
-                } catch (Exception e) {
-                    Log.d("ERR", e.toString());
-                    result = e.toString();
-                }
-
-
-            }
-
-        });
-        thread.start();
+    private void toggleFab() {
+        if (isFabOpen) {
+            fab_main.setImageResource(R.drawable.ic_more);
+            fab_sub1.startAnimation(fab_close);
+            fab_sub1.setClickable(false);
+            isFabOpen = false;
+        } else {
+            fab_main.setImageResource(R.drawable.ic_close);
+            fab_sub1.startAnimation(fab_open);
+            fab_sub1.setClickable(true);
+            isFabOpen = true;
+        }
     }
-
 }
